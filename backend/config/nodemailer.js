b2cf4ee699a52d187591a245f0c542c5e4242738
Dhairya <dnshah5115@gmail.com>
2026-07-04 22:24:54 +0530

    sa

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+    port: parseInt(process.env.EMAIL_PORT, 10) || 587,
+    secure: process.env.EMAIL_SECURE === 'true' || false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
